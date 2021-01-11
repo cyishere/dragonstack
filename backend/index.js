b0b8470a53f471059652fa3923854bf5303d4e76
irini <cyishere@gmail.com>
2021-01-11 19:23:20 +0800

    setup basic express

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
+const express = require("express");
 const GenerationEngine = require("./engine");
 
+const app = express();
 const engine = new GenerationEngine();
+const port = 3001;
 
 engine.start();
 
-setTimeout(() => {
-  engine.stop();
-}, 20000);
+app.get("/dragon/new", (req, res) => {
+  res.json({ dragon: engine.generation.newDragon() });
+});
+
+app.listen(port, () =>
+  console.log(`🚀 server running on http://localhost:${port}`)
+);
