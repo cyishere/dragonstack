db9454d675a109642edd3358bc408c48e20baf59
irini <cyishere@gmail.com>
2021-01-11 20:09:32 +0800

    add api

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const GenerationEngine = require("./generation/engine");
+const dragonRouter = require("./api/dragon");
+const generationRouter = require("./api/generation");
 
 const app = express();
 const engine = new GenerationEngine();
 
-engine.start();
+// make `engine` global in the app
+app.locals.engine = engine;
 
-app.get("/dragon/new", (req, res) => {
-  res.json({ dragon: engine.generation.newDragon() });
-});
+/**
+ * Routes
+ */
+app.use("/dragon", dragonRouter);
+app.use("/generation", generationRouter);
+
+engine.start();
 
 module.exports = app;
