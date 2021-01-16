ccc773c95a3d5b9df1f127b0551fa6f306fa340a
irini <cyishere@gmail.com>
2021-01-16 21:18:01 +0800

    trait table & get trait id

--- backend/bin/insertTraits.js
+++ backend/bin/insertTraits.js
@@ -8,15 +8,14 @@ TRAITS.forEach((TRAIT) => {
   traitValues.forEach((traitValue) => {
     pool.query(
       `INSERT INTO trait("traitType", "traitValue")
-       VALUES($1, $2)
-       RETURNING id`,
+       VALUES($1, $2) RETURNING id`,
       [traitType, traitValue],
       (error, response) => {
         if (error) console.error(error);
 
         const traitId = response.rows[0].id;
 
-        console.log(`Inserted trait - id: ${traitId}`);
+        console.log(`Inserted id - traitId: ${traitId}`);
       }
     );
   });
