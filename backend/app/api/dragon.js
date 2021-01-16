fca508c0ce7cb71a5f72da7f814fdae8219bbc04
irini <cyishere@gmail.com>
2021-01-16 11:54:26 +0800

    store dragon to database

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,8 +1,19 @@
 const { Router } = require("express");
 const router = new Router();
+const DragonTable = require("../dragon/table");
 
 router.get("/new", (req, res) => {
-  res.json({ dragon: req.app.locals.engine.generation.newDragon() });
+  const dragon = req.app.locals.engine.generation.newDragon();
+
+  DragonTable.storeDragon(dragon)
+    .then(({ dragonId }) => {
+      console.log("dragonId", dragonId);
+
+      dragon.dragonId = dragonId;
+
+      res.json({ dragon });
+    })
+    .catch((error) => console.error(error));
 });
 
 module.exports = router;
