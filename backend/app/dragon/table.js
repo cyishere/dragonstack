e2f96352b11f2b590d70f60c67c0116f4cdaf879
irini <cyishere@gmail.com>
2021-01-19 10:51:07 +0800

    add getDragon function to DragonTable

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -30,6 +30,25 @@ class DragonTable {
       );
     });
   }
+
+  static getDragon({ dragonId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT birthdate, nickname, "generationId"
+         FROM dragon
+         WHERE dragon.id = $1`,
+        [dragonId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          if (response.rows.length === 0) return reject(new Error("no dragon"));
+
+          // return an object contains "birthdate, nickname, generationId"
+          resolve(response.rows[0]);
+        }
+      );
+    });
+  }
 }
 
 module.exports = DragonTable;
