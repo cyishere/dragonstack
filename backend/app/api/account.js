30f6ddaeefc7441573fbbb4a52dd9aa95a39ef01
irini <cyishere@gmail.com>
2021-01-26 20:00:37 +0800

    store hashed username & password

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 const AccountTable = require("../account/table");
+const { hash } = require("../account/helper");
 
 const router = new Router();
 
 router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
 
-  AccountTable.storeAccount({ username, password })
+  const usernameHash = hash(username);
+  const passwordHash = hash(password);
+
+  AccountTable.storeAccount({ usernameHash, passwordHash })
     .then(() => res.json({ message: "success!" }))
     .catch((error) => next(error));
 });
