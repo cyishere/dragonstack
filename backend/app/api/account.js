a0f6fee3a367beb8493ce414c5b449e75b8d484d
irini <cyishere@gmail.com>
2021-01-28 10:29:20 +0800

    add  in backend

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -75,4 +75,28 @@ router.get("/logout", (req, res, next) => {
     .catch((error) => next(error));
 });
 
+/**
+ * Authenticated
+ */
+router.get("/authenticated", (req, res, next) => {
+  const { sessionString } = req.cookies;
+
+  if (!sessionString || !Session.verify(sessionString)) {
+    const error = new Error("Invalide session.");
+
+    error.statusCode = 400;
+
+    return next(error);
+  } else {
+    const { username, id } = Session.parse(sessionString);
+
+    AccountTable.getAccount({ usernameHash: hash(username) })
+      .then(({ account }) => {
+        const authenticated = account.sessionId === id;
+        res.json({ authenticated });
+      })
+      .catch((error) => next(error));
+  }
+});
+
 module.exports = router;
