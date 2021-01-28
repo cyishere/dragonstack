4f8624a13980658551fcca8bd8e19283b7b96f44
irini <cyishere@gmail.com>
2021-01-28 16:35:07 +0800

    refactor New Dragon in backend

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,18 +1,31 @@
 const { Router } = require("express");
+const AccountDragonTable = require("../accountDragon/table");
 const router = new Router();
 const DragonTable = require("../dragon/table");
+const { authenticatedAccount } = require("./helper");
 
 router.get("/new", (req, res, next) => {
-  const dragon = req.app.locals.engine.generation.newDragon();
+  let accountId, dragon;
 
-  DragonTable.storeDragon(dragon)
+  authenticatedAccount({ sessionString: req.cookies.sessionString })
+    .then(({ account }) => {
+      accountId = account.id;
+
+      dragon = req.app.locals.engine.generation.newDragon();
+
+      return DragonTable.storeDragon(dragon);
+    })
     .then(({ dragonId }) => {
       console.log("dragonId", dragonId);
 
+      // ❗ There's no need to retrieve all the data(objects) from database,
+      //    we only need the auto generated one - dragonId(id) - others are
+      //    already in the `dragon` object.
       dragon.dragonId = dragonId;
 
-      res.json({ dragon });
+      return AccountDragonTable.storeAccountDragon({ accountId, dragonId });
     })
+    .then(() => res.json({ dragon }))
     .catch((error) => next(error));
 });
 
