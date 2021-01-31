5c461a5aacf138a6c008d8a261ed57e0257b7711
irini <cyishere@gmail.com>
2021-01-31 11:14:27 +0800

    pick traits for breeding baby

--- backend/app/dragon/breeder.js
+++ backend/app/dragon/breeder.js
@@ -1,3 +1,4 @@
+const base64 = require("base-64");
 const Dragon = require("./index");
 
 class Breeder {
@@ -11,8 +12,8 @@ class Breeder {
       const matronTrait = traitValue;
 
       const patronTrait = patronTraits.find(
-        (trait) => trait.traitValue === traitValue
-      );
+        (trait) => trait.traitType === traitType
+      ).traitValue;
 
       babyTraits.push({
         traitType,
@@ -24,6 +25,34 @@ class Breeder {
       nickname: "Unnamed baby",
       traits: babyTraits,
     });
+  }
+
+  /**
+   * Two incoming traits: matronTrait and patronTrait
+   * The matronTrait and patronTrait string values are encoded.
+   * Both traits have their characters summed.
+   * Get a range by adding both character sums.
+   * Generate a random number in that range.
+   * If the number is less than the matron's sum, pick matron.
+   * Else, pick patron.
+   */
+  static pickTrait({ matronTrait, patronTrait }) {
+    if (matronTrait === patronTrait) return matronTrait;
+
+    const matronTraitCharSum = Breeder.charSum(base64.encode(matronTrait));
+    const patronTraitCharSum = Breeder.charSum(base64.encode(patronTrait));
+
+    const randNum = Math.floor(
+      Math.random() * (matronTraitCharSum + patronTraitCharSum)
+    );
+
+    return randNum < matronTraitCharSum ? matronTrait : patronTrait;
+  }
+
+  static charSum(string) {
+    return string
+      .split("")
+      .reduce((sum, character) => (sum += character.charCodeAt()), 0);
   }
 }
 
