8f08c5b01d071c8d58d9f072e639909ec7c87796
irini <cyishere@gmail.com>
2021-01-31 18:00:09 +0800

    limit dragons per generation

--- frontend/src/components/Dragon.js
+++ frontend/src/components/Dragon.js
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDragon } from "../slices/dragonSlice";
 import DragonAvatar from "./DragonAvatar";
+import fetchStates from "../slices/fetchStates";
 
 const Dragon = () => {
   const dragon = useSelector((state) => state.dragon);
@@ -18,7 +19,12 @@ const Dragon = () => {
       <Button bsStyle="primary" onClick={handleClickNew}>
         New Dragon
       </Button>
-      <DragonAvatar dragon={dragon} />
+
+      {dragon.status === fetchStates.error ? (
+        <p style={{ color: "red" }}>{dragon.message}</p>
+      ) : (
+        <DragonAvatar dragon={dragon} />
+      )}
     </div>
   );
 };
