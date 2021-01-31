9bfadd1c0bb3372cea627a71ea9d2d3686ba362e
irini <cyishere@gmail.com>
2021-01-31 10:43:55 +0800

    add breedDragon methond

---
+++ backend/app/dragon/breeder.js
@@ -0,0 +1,30 @@
+const Dragon = require("./index");
+
+class Breeder {
+  static breedDragon({ matron, patron }) {
+    const matronTraits = matron.traits;
+    const patronTraits = patron.traits;
+
+    const babyTraits = [];
+
+    matronTraits.forEach(({ traitType, traitValue }) => {
+      const matronTrait = traitValue;
+
+      const patronTrait = patronTraits.find(
+        (trait) => trait.traitValue === traitValue
+      );
+
+      babyTraits.push({
+        traitType,
+        traitValue: Breeder.pickTrait({ matronTrait, patronTrait }),
+      });
+    });
+
+    return new Dragon({
+      nickname: "Unnamed baby",
+      traits: babyTraits,
+    });
+  }
+}
+
+module.exports = Breeder;
